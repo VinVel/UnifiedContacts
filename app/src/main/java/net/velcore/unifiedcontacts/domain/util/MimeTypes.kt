badc7dd042ec4f2d82b6ea48c4dd1898eee2526f
git <git@velcore.net>
2026-01-29 00:25:08 +0100

    Declare MimeTypes

--- app/src/main/java/net/velcore/unifiedcontacts/domain/util/MimeTypes.kt
+++ app/src/main/java/net/velcore/unifiedcontacts/domain/util/MimeTypes.kt
@@ -12,4 +12,16 @@
 package net.velcore.unifiedcontacts.domain.util
 
 object MimeTypes {
+    const val PHONE = "vnd.android.cursor.item/phone_v2"
+    const val EMAIL = "vnd.android.cursor.item/email_v2"
+    const val POSTAL_ADDRESS = "vnd.android.cursor.item/postal-address_v2"
+    const val EVENT = "vnd.android.cursor.item/contact_event"
+    const val ORGANIZATION = "vnd.android.cursor.item/organization"
+    const val NOTE = "vnd.android.cursor.item/note"
+    const val NAME = "vnd.android.cursor.item/name"
+    const val WEBSITE = "vnd.android.cursor.item/website"
+    const val RELATION = "vnd.android.cursor.item/relation"
+    const val PHOTO = "vnd.android.cursor.item/photo"
+    const val GROUP = "vnd.android.cursor.item/group_membership"
+    const val READ_ONLY = "vnd.android.cursor.item/vnd.net.velcore.unifiedcontacts.read-only"
 }

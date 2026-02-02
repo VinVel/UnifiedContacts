0e1503b02bb20c0400513fc7dec230a9d89d44b9
git <git@velcore.net>
2026-02-02 23:51:10 +0100

    Overhaul license headers

--- app/src/main/java/net/velcore/unifiedcontacts/ui/viewmodel/MirrorViewModel.kt
+++ app/src/main/java/net/velcore/unifiedcontacts/ui/viewmodel/MirrorViewModel.kt
@@ -6,7 +6,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * Project home: https://unifiedcontacts.velcore.net/
+ * Project home: unifiedcontacts.velcore.net
  */
 
 package net.velcore.unifiedcontacts.ui.viewmodel

6fb3e10d858ca28177594a8115a6f02df97f3cf2
git <git@velcore.net>
2026-02-06 20:00:10 +0100

    Initial commit

---
+++ app/src/main/java/net/velcore/unifiedcontacts/system/mirror/MirrorCreator.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026 VinVel
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Project home: https://unifiedcontacts.velcore.net/
+ */
+
+package net.velcore.unifiedcontacts.system.mirror
+
+class MirrorCreator {
+}

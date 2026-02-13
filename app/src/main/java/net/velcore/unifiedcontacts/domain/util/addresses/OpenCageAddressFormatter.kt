bc092d522fe44194bb7cd46efa8fb38f9ace2402
git <git@velcore.net>
2026-02-13 03:09:18 +0100

    Add missing license Header

--- app/src/main/java/net/velcore/unifiedcontacts/domain/util/addresses/OpenCageAddressFormatter.kt
+++ app/src/main/java/net/velcore/unifiedcontacts/domain/util/addresses/OpenCageAddressFormatter.kt
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2026 VinVel
+ * SPDX-License-Identifier: MPL-2.0
+ * Incompatible With Secondary Licenses.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Project home: unifiedcontacts.velcore.net
+ */
+
 package net.velcore.unifiedcontacts.domain.util.addresses
 
 import java.util.Locale

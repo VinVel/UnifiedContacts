c5e19b7fb4cabb1a9e173ad5f97b0bf1ac4f043e
git <git@velcore.net>
2026-02-13 02:33:50 +0100

    Add "Incompatible With Secondary Licenses" notice and gradle stuff

--- app/src/main/java/net/velcore/unifiedcontacts/domain/tags/TagRules.kt
+++ app/src/main/java/net/velcore/unifiedcontacts/domain/tags/TagRules.kt
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2026 VinVel
  * SPDX-License-Identifier: MPL-2.0
+ * Incompatible With Secondary Licenses.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this

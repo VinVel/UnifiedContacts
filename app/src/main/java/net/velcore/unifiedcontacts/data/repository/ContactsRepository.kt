2eddeaf9d66d25dd113f483c785a3abb9b7543ef
git <git@velcore.net>
2026-03-05 02:54:44 +0100

    Define a way to get a Contact List of all Contacts and their primary Displayname

--- app/src/main/java/net/velcore/unifiedcontacts/data/repository/ContactsRepository.kt
+++ app/src/main/java/net/velcore/unifiedcontacts/data/repository/ContactsRepository.kt
@@ -12,5 +12,23 @@
 
 package net.velcore.unifiedcontacts.data.repository
 
-class ContactsRepository {
+import android.accounts.Account
+import android.content.Context
+import contacts.core.Contacts
+import contacts.core.ContactsFields
+import contacts.core.asc
+import net.velcore.unifiedcontacts.domain.contact.AccountRef
+
+class ContactsRepository(private val context: Context) {
+    fun getAllContactNames(accountRef: AccountRef? = null): List<String> {
+        val query = Contacts(context)
+            .query()
+            .orderBy(ContactsFields.DisplayNamePrimary.asc())
+
+        val contacts =
+            if (accountRef != null) {query.accounts(Account(accountRef.name, accountRef.type)).find()}
+            else {query.find()} //this is useful for when the user wants to view their contact from all Accounts simultaneously
+
+        return contacts.mapNotNull { it.displayNamePrimary }
+    }
 }
